482216e55ec4adde55ed1a93b1ab9c6f9aa94d1f
CaseyFroehlich <caseyfroehlich8559@gmail.com>
2023-02-14 10:49:47 -0500

    Adjust styling home page

--- ngCommunityUnited/src/app/components/home/home.component.ts
+++ ngCommunityUnited/src/app/components/home/home.component.ts
@@ -14,10 +14,10 @@ import { NgFor, NgIf } from '@angular/common';
 	providers: [NgbCarouselConfig], // add NgbCarouselConfig to the component providers
 })
 export class HomeComponent implements OnInit {
-	showNavigationArrows = true;
+	showNavigationArrows = false;
 	showNavigationIndicators = true;
 	// images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  images = ['https://images.pexels.com/photos/6994992/pexels-photo-6994992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 'https://www.greekyearbook.com/wp-content/uploads/2016/06/795x341_Volunteer_BlogHeader.jpg'];
+  images = ['https://images.pexels.com/photos/6994992/pexels-photo-6994992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 'https://images.pexels.com/photos/6646918/pexels-photo-6646918.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2','https://images.pexels.com/photos/9037596/pexels-photo-9037596.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'];
 
 
   ngOnInit(){
@@ -27,7 +27,7 @@ export class HomeComponent implements OnInit {
 
 	constructor(config: NgbCarouselConfig, private auth: AuthService) {
 		// customize default values of carousels used by this component tree
-		config.showNavigationArrows = true;
+		config.showNavigationArrows = false;
 		config.showNavigationIndicators = true;
 	}
 }
